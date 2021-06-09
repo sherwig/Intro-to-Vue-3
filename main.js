f09ec73d44c3b1d9810d597903edb8fcbf5950da
Sam <sahe2032@colorado.edu>
2021-06-09 14:48:56 -0600

    end of 3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://github.com/Code-Pop/Intro-to-Vue-3/branches/active'
     }
+  }
 })
